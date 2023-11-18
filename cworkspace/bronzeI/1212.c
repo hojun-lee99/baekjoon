6afe39b72c5b26bd01d43124666ebd553e162bfc
magik99 <hojun0810@naver.com>
2023-11-18 23:12:25 +0900

    solved 1212
    1212번 문제 8진수 2진수 변환

--- cworkspace/bronzeI/1212.c
+++ cworkspace/bronzeI/1212.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
-    int ox;
-    scanf("%o", &ox);
+    char dec[1000003] = { 0 };
+    char oct[333335] = { 0 };
 
-    printf("")
+    scanf("%s", oct);
+    int len = strlen(oct);
+
+    int count = 0;
+    if (oct[0]<50)
+    {
+    dec[count++] = oct[0]%2;        
+    }
+    else if (oct[0]<52)
+    {
+        dec[count++] = oct[0]/2%2;
+        dec[count++] = oct[0]%2;
+    }
+    else
+    {
+        dec[count++] = (oct[0]-'0')/2/2;
+        dec[count++] = (oct[0]-'0')/2%2;
+        dec[count++] = (oct[0]-'0')%2;
+    }
+
+    for (int i = 1; i < len; i++)
+    {
+        dec[count++] = (oct[i]-'0')/2/2;
+        dec[count++] = (oct[i]-'0')/2%2;
+        dec[count++] = (oct[i]-'0')%2;
+    }
+    
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d", dec[i]);
+    }
+    
 }
