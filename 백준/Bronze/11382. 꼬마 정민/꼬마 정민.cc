140e4377496dc36d6e1aa68e7b65bd5a4bad4c0a
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2024-08-11 16:28:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main () {
+    long long int A, B, C;
+    long long int sum;
+
+    std::cin >> A >> B >> C;
+
+    sum = A + B + C;
+
+    std::cout << sum;
+
+    return 0;
+}
