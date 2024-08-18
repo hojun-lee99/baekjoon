e80f4b50652fef8be493b8beb244ca44983cd5f0
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2024-08-18 15:56:29 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    string a, p, b;
+
+    cin >> a >> p;
+    
+    cout << p;
+    
+}
