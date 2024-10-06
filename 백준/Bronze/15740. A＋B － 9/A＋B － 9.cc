dbcc25a69f5ca19146271fcd46dac681e49dce34
magik <55483831+hojun-lee99@users.noreply.github.com>
2024-10-06 18:04:00 +0900

    [Bronze V] Title: A+B - 9, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+    int a, b;
+    int sum;
+
+    std::cin >> a >> b;
+
+    sum = a + b;
+
+    std::cout << sum;
+
+}
